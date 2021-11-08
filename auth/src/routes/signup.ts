aaa036a7b9b38a6037ddadf8a5a9f475a4ac4939
murtalaliyu <murtalaliyu10@gmail.com>
2021-11-07 20:40:32 -0500

    added password hashing for users

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,8 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
+import { User } from '../models/user';
 import { RequestValidationError } from '../errors/request-validation-error';
-import { DatabaseConnectionError } from '../errors/database-connection-error';
+import { BadRequestError } from '../errors/bad-request-error';
 
 const router = express.Router();
 
@@ -19,16 +20,24 @@ router.post(
   async (req: Request, res: Response) => {
     const errors = validationResult(req);
 
+    // invalid parameters
     if (!errors.isEmpty()) {
       throw new RequestValidationError(errors.array());
     }
 
+    // check if email already exists
     const { email, password } = req.body;
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      throw new BadRequestError('Email in use');
+    }
 
-    console.log('Creating a user...');
-    throw new DatabaseConnectionError();
+    // create User and save to db
+    const user = User.build({ email, password });
+    await user.save();
 
-    res.send({});
+    // respond
+    res.status(201).send(user);
 });
 
 export { router as signupRouter };
