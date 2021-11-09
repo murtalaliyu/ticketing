d87de277fe9ac7f741a7ee76f7600196aeee925e
murtalaliyu <murtalaliyu10@gmail.com>
2021-11-09 00:11:17 -0500

    working on adding JWT

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
+import jwt from 'jsonwebtoken';
+
 import { User } from '../models/user';
 import { RequestValidationError } from '../errors/request-validation-error';
 import { BadRequestError } from '../errors/bad-request-error';
@@ -35,6 +37,12 @@ router.post(
     // create User and save to db
     const user = User.build({ email, password });
     await user.save();
+
+    // generate JWT
+    
+    
+    // store JWT on the session object
+
 
     // respond
     res.status(201).send(user);
