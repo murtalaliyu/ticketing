f67e90eb9b7edfa9e0d95ca896ba6da3315cda9c
murtalaliyu <murtalaliyu10@gmail.com>
2021-11-16 20:24:24 -0500

    deleted duplicate code, moved to npm package named common

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 
-import { currentUser } from '../middlewares/current-user';
+import { currentUser } from '@bluepink-tickets/common';
 
 const router = express.Router();
 
