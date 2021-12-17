e813f678f61660c8f0f32d9f4f76531ea49bf671
murtalaliyu <murtalaliyu10@gmail.com>
2021-12-16 19:00:46 -0500

    quick test

--- payments/src/stripe.ts
+++ payments/src/stripe.ts
@@ -1,6 +1,6 @@
 import Stripe from 'stripe';
 
-export const stripe = new Stripe(process.env.STRIPE_KEY!, {
+export const stripe = new Stripe('sk_test_51K6MRoKO0cS0QXukigsFHZomTuyc8zzODxxW1SOF2JmeBEp4Mp234xJvqt3huGkqaW5pZxm3w0CIwjK98xrpSYCq00eb1X6lJW', {
   apiVersion: '2020-08-27'
 });
 
